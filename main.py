5d00c1e35a725615f8c34aa7eda766d2265a779e
GOD OF THUNDER <93386525+yadhuhari@users.noreply.github.com>
2023-06-29 13:30:39 +0530

    Update main.py

--- main.py
+++ main.py
@@ -3,7 +3,9 @@ from discord.ext import commands
 
 intents = discord.Intents.default()
 intents.message_content = True
-client = commands.Client(command_prefix = '/', self_bot=True, intents=intents)
+client = commands.Bot(command_prefix = '/', self_bot=True, intents=intents)
+
+DISCORD_TOKEN={MTEyMzYxNjAyMDEyNTQ1NDQ0OA.GfR9TJ.9J7Bepjr6RtNgRAgFA-syHm7YV5TLeddso7Q2g}
 
 @client.event
 async def on_ready():
@@ -20,4 +22,4 @@ Just send me a direnct link to convert it into a file..!!
 Made By BMZ ❤"""
     )
                    
-client.run("MTEyMzYxNjAyMDEyNTQ1NDQ0OA.Gu-eBE.tl9_FuduZ8-z7y1uP2_r-orYBgDbjZflhA8fZo")
+client.run(DISCORD_TOKEN)
