809e93d9a5f99809b2896e2422b19830ca6539ef
GOD OF THUNDER <93386525+yadhuhari@users.noreply.github.com>
2023-06-29 13:03:21 +0530

    Update main.py

--- main.py
+++ main.py
@@ -4,14 +4,14 @@ from discord.ext import commands
 
 intents = discord.Intents.default()
 intents.message_content = True
-bot = commands.Bot(command_prefix = '/', intents=intents)
+client = commands.Bot(command_prefix = '/', intents=intents)
 
-@bot.event
+@client.event
 async def on_ready():
     print("Bot Started..!!")
     print("---------------")
 
-@bot.command(pass_context=True)
+@client.command(pass_context=True)
 async def start(ctx):
     await ctx.send("""Hello {ctx.message.author.mention}
 
@@ -21,4 +21,4 @@ Just send me a direnct link to convert it into a file..!!
 Made By BMZ ❤"""
     )
                    
-bot.run("MTEyMzYxNjAyMDEyNTQ1NDQ0OA.Gu-eBE.tl9_FuduZ8-z7y1uP2_r-orYBgDbjZflhA8fZo", bot=True)
+client.run("MTEyMzYxNjAyMDEyNTQ1NDQ0OA.Gu-eBE.tl9_FuduZ8-z7y1uP2_r-orYBgDbjZflhA8fZo", bot=True)
