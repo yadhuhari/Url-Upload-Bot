9804f59bf1c6b2eb7415a4f8da1670c5fe2959d0
GOD OF THUNDER <93386525+yadhuhari@users.noreply.github.com>
2023-06-29 12:31:55 +0530

    Update main.py

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from discord.ext import commands
 
 intents = discord.Intents.default()
 intents.message_content = True
-client = MyClient(intents=intents)
+client = commands.Bot(command_prefix = '/')
 
 @client.event
 async def on_ready():
