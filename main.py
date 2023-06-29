c0395e93370a80aaf783366d64d7c17b71454b37
GOD OF THUNDER <93386525+yadhuhari@users.noreply.github.com>
2023-06-29 16:53:46 +0530

    Update main.py

--- main.py
+++ main.py
@@ -3,16 +3,16 @@ from discord.ext import commands
 
 intents = discord.Intents.default()
 intents.message_content = True
-client = commands.Bot(command_prefix = '/', self_bot=True, intents=intents)
+bot = commands.Bot(command_prefix = '/', self_bot=True, intents=intents, pm_help=True, case_insensitive=True)
 
-DISCORD_TOKEN="MTEyMzYxNjAyMDEyNTQ1NDQ0OA.GfR9TJ.9J7Bepjr6RtNgRAgFA-syHm7YV5TLeddso7Q2g"
+DISCORD_TOKEN = "MTEyMzYxNjAyMDEyNTQ1NDQ0OA.GfR9TJ.9J7Bepjr6RtNgRAgFA-syHm7YV5TLeddso7Q2g"
 
-@client.event
+@bot.event
 async def on_ready():
     print("Bot Started..!!")
     print("---------------")
 
-@client.command(pass_context=True)
+@bot.command(pass_context=True)
 async def start(ctx):
     await ctx.send("""Hello {ctx.message.author.mention}
 
@@ -21,5 +21,5 @@ Just send me a direnct link to convert it into a file..!!
 
 Made By BMZ ❤"""
     )
-                   
-client.run(DISCORD_TOKEN)
+        
+bot.run(DISCORD_TOKEN, bot=True, reconnect=True)
